dc8a606ec375fc0c900fef764cae175ff32d7d83
ryanutah <kledzikryan@gmail.com>
2025-09-20 15:14:44 -0700

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 const lightTheme = "styles/light.css";
 const darkTheme = "styles/dark.css";
 const sunIcon = "assets/SunIcon.svg";
@@ -18,20 +19,15 @@ function calculate(value) {
   }
 }
 
-// Swaps the stylesheet to achieve dark mode.
+// Swaps the stylesheet to achieve dark mode (text labels removed).
 function changeTheme() {
   const theme = document.getElementById("theme");
-  setTimeout(() => {
-    toast.innerHTML = "Calculator";
-  }, 1500);
   if (theme.getAttribute("href") === lightTheme) {
     theme.setAttribute("href", darkTheme);
     themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
   } else {
     theme.setAttribute("href", lightTheme);
     themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
   }
 }
 
@@ -48,12 +44,8 @@ document.addEventListener("keydown", keyboardInputHandler);
 
 //function to handle keyboard inputs
 function keyboardInputHandler(e) {
-  // to fix the default behavior of browser,
-  // enter and backspace were causing undesired behavior when some key was already in focus.
   e.preventDefault();
-  //grabbing the liveScreen
 
-  //numbers
   if (e.key === "0") {
     res.value += "0";
   } else if (e.key === "1") {
@@ -70,15 +62,12 @@ function keyboardInputHandler(e) {
     res.value += "6";
   } else if (e.key === "7") {
     res.value += "7";
-  } else if (e.key === "7") {
-    res.value += "7";
   } else if (e.key === "8") {
     res.value += "8";
   } else if (e.key === "9") {
     res.value += "9";
   }
 
-  //operators
   if (e.key === "+") {
     res.value += "+";
   } else if (e.key === "-") {
@@ -89,20 +78,16 @@ function keyboardInputHandler(e) {
     res.value += "/";
   }
 
-  //decimal key
   if (e.key === ".") {
     res.value += ".";
   }
 
-  //press enter to see result
   if (e.key === "Enter") {
     calculate(result.value);
   }
 
-  //backspace for removing the last input
   if (e.key === "Backspace") {
     const resultInput = res.value;
-    //remove the last element in the string
     res.value = resultInput.substring(0, res.value.length - 1);
   }
 }
